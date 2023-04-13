aed480b1045393d2c583e62f3401c3ac13bbab54
jammln <129803361+jammln@users.noreply.github.com>
2023-04-13 17:39:40 +0900

    Add files via upload

---
+++ Decryptor_app/Decryptor_app/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace Decryptor_app
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Browse = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Decryption = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Browse
+            // 
+            this.Browse.Location = new System.Drawing.Point(406, 140);
+            this.Browse.Name = "Browse";
+            this.Browse.Size = new System.Drawing.Size(83, 23);
+            this.Browse.TabIndex = 0;
+            this.Browse.Text = "Browse...";
+            this.Browse.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(96, 145);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Browse your encrypted file path...";
+            // 
+            // Decryption
+            // 
+            this.Decryption.Location = new System.Drawing.Point(266, 280);
+            this.Decryption.Name = "Decryption";
+            this.Decryption.Size = new System.Drawing.Size(223, 33);
+            this.Decryption.TabIndex = 3;
+            this.Decryption.Text = "Decryption Now";
+            this.Decryption.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 193);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(349, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Note: If you delete key.bin or iv.bin to can\'t recover i\'m sorry";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 325);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Decryption);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Browse);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Archiver Ransomware Decryption Tool";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Browse;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Decryption;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Decryptor_app/Decryptor_app/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace Decryptor_app
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.FixedSingle; // 또는 FormBorderStyle.FixedDialog
+            // 버튼의 Click 이벤트 핸들러를 설정합니다.
+            Browse.Click += new EventHandler(Browse_Click);
+            Decryption.Click += new EventHandler(Decryption_Click);
+        }
+        private void Browse_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                label1.Text = dialog.SelectedPath;
+            }
+        }
+        private void Decryption_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (label1.Text == "Browse your encrypted file path...")
+                {
+                    MessageBox.Show("Select Decryption Folder path");
+                }
+                else if (!Directory.GetFiles(label1.Text, "*.!Locked", SearchOption.AllDirectories).Any() ||
+                !File.Exists(Path.Combine(label1.Text, "key.bin")) ||
+                !File.Exists(Path.Combine(label1.Text, "iv.bin")))
+                {
+                    MessageBox.Show("No files to decrypt");
+                }
+                else
+                {
+                    // Read encryption key and initialization vector
+                    string directoryPath = label1.Text;
+                    byte[] key = File.ReadAllBytes(Path.Combine(directoryPath, "key.bin"));
+                    byte[] iv = File.ReadAllBytes(Path.Combine(directoryPath, "iv.bin"));
+
+                    // Get all encrypted files in the directory
+                    string[] filePaths = Directory.GetFiles(directoryPath, "*.!Locked", SearchOption.AllDirectories);
+
+                    // Create an instance of the AES encryption algorithm
+                    using (Aes aes = Aes.Create())
+                    {
+                        // Set encryption key and initialization vector
+                        aes.Key = key;
+                        aes.IV = iv;
+
+                        // Delete !INFO!.txt file
+                        string infoFilePath = Path.Combine(directoryPath, "!INFO!.txt");
+                        if (File.Exists(infoFilePath))
+                        {
+                            File.Delete(infoFilePath);
+                        }
+
+                        // Decrypt all files in the directory
+                        foreach (string encryptedFilePath in filePaths)
+                        {
+                            // Separate file name and extension
+                            string fileName = Path.GetFileNameWithoutExtension(encryptedFilePath).Replace(".!Locked", "");
+                            string fileExtension = Path.GetExtension(fileName);
+
+                            // Create decrypted file path
+                            string decryptedFilePath = Path.Combine(Path.GetDirectoryName(encryptedFilePath), fileName);
+
+                            //Add string iv and key
+                            string IVfilepath = Path.Combine(directoryPath, "iv.bin");
+                            string Keyfilepath = Path.Combine(directoryPath, "key.bin");
+
+                            // Create file streams
+                            using (FileStream encryptedFileStream = new FileStream(encryptedFilePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                            {
+                                using (FileStream decryptedFileStream = new FileStream(decryptedFilePath, FileMode.Create, FileAccess.Write, FileShare.Read))
+                                {
+                                    // Create decryption stream
+                                    using (ICryptoTransform decryptor = aes.CreateDecryptor())
+                                    {
+                                        // Decrypt the file contents and write to the decrypted file stream
+                                        using (CryptoStream cryptoStream = new CryptoStream(encryptedFileStream, decryptor, CryptoStreamMode.Read))
+                                        {
+                                            cryptoStream.CopyTo(decryptedFileStream);
+                                        }
+                                    }
+                                }
+                            }
+
+                            // Delete the encrypted file
+                            File.Delete(encryptedFilePath);
+                            File.Delete(IVfilepath);
+                            File.Delete(Keyfilepath);
+                        }
+                    }
+                    MessageBox.Show("Decryption is completed!");
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                MessageBox.Show("Access is denied.");
+            }
+        }
+    }
+}
